453cd53a712c99da658c7fb64a61df55a7a81f76
Abhiram Laha <90309476+Abhiram-Laha@users.noreply.github.com>
2021-11-19 12:36:48 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,30 @@
+""" Password Validator  """
+
+print("------PASSWORD VALIDATOR-----\n")
+password = input("Enter your Password : ");
+print()
+
+sym = ('@',"#","$","*","!","~","&")
+
+count_digit=0;
+count_spec=0;
+cout_upper=0;
+lent=len(password)
+
+if lent < 4:
+    print("WEAK PASSWORD")
+    
+for i in password:
+    if i.isdigit():
+        count_digit+=1;
+    if i in sym:
+        count_spec+=1;
+    if i.isupper():
+        cout_upper+=1;
+        
+
+if (count_digit>2 and count_spec>0 and cout_upper>0):
+    print("STRONG PASSWORD")
+    
+
+        
